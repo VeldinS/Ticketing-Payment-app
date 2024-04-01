ab212227bf762d40190a1ba22d442e309fff71d7
VeldinSal <veldinsalcinovic123@gmail.com>
2024-04-01 13:44:00 +0200

    Subclassing for custom errors

---
+++ auth/src/errors/request-validation-error.ts
@@ -0,0 +1,9 @@
+import {ValidationError, validationResult} from "express-validator";
+
+export class RequestValidationError extends Error{
+    constructor(public errors: ValidationError[]) {
+        super();
+
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+}
